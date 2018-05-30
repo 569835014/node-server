c2750f34af3c13c91c645b8ba954d30a07e8b3d3
minzhang <569835014@qq.com>
2018-05-30 08:59:47 +0800

    初始化

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import Koa from 'koa2'
+import R from 'ramda'
+console.info(process.env.NODE_ENV)
+const MIDDLEWARES = ['database', 'common', 'router','static']
+
+class Server{
+    constructor(){
+        this.app=new Koa();
+        this.useMiddleWares(this.app)(MIDDLEWARES)
+    }
+    useMiddleWares(app) {
+        return R.map(R.compose(
+            R.map(i => i(app)),
+            require,
+            i => `${r('./middlewares')}/${i}`
+        ))
+    }
+}
