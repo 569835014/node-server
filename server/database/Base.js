e316c30d73164a01f03b5ac632d9d9fe868500c7
minzhang <569835014@qq.com>
2018-06-03 00:15:17 +0800

    分离模型层和dao层

--- server/database/Base.js
+++ server/database/Base.js
@@ -21,5 +21,60 @@ class ModelBase {
     off(...arg){
         this.model.removeHook.apply(this,arg)
     }
+    async handle(name,params){
+        if(params.scope){
+            return await this.model.scope(params.scope)[name](params)
+        }
+        return await this.model[name](params)
+    }
+    async findById(params){
+        return await this.handle('findById',params)
+    }
+    async findOne(params){
+        return await this.handle('findOne',params)
+    }
+    async findOrCreate(params){
+        return await this.handle('findOne',params)
+    }
+    async findAndCountAll(params){
+        return await this.handle('findAndCountAll',params)
+    }
+    async findAll(params){
+        return await this.handle('findAll',params)
+    }
+    async count(params){
+        return await this.handle('count',params)
+    }
+    async max(params){
+        return await this.handle('max',params)
+    }
+    async min(params){
+        return await this.handle('min',params)
+    }
+    async sum(params){
+        return await this.handle('sum',params)
+    }
+
+    async delete(params){
+        return await this.handle('delete',params)
+    }
+    async deleteById(params){
+        let model= await this.handle('findById',params)
+        model.destroy();
+    }
+
+    async query(params){
+        this.database.query(params)
+    }
+    //合并数据库操作添加到事物中
+    async transaction(dataBaseFn){
+        let transaction=await this.database.transaction()
+        try {
+            await dataBaseFn();
+        }catch (e) {
+            return await transaction.rollback()
+        }
+        return await transaction.commit()
+    }
 }
 export default ModelBase
--- server/database/BaseDao.js
+++ server/database/BaseDao.js
@@ -1,66 +1,31 @@
 class BaseDao {
-    constructor(database,model){
+    constructor(database,model,params){
         this.model=model;
+        this.dao=this.create(params)
         this.database=database
     }
-    async handle(name,params){
-        if(params.scope){
-            return await this.model.scope(params.scope)[name](params)
-        }
-        return await this.model[name](params)
-    }
-    async findById(params){
-        return await this.handle('findById',params)
-    }
-    async findOne(params){
-        return await this.handle('findOne',params)
-    }
-    async findOrCreate(params){
-        return await this.handle('findOne',params)
-    }
-    async findAndCountAll(params){
-        return await this.handle('findAndCountAll',params)
-    }
-    async findAll(params){
-        return await this.handle('findAll',params)
-    }
-    async count(params){
-        return await this.handle('count',params)
-    }
-    async max(params){
-        return await this.handle('max',params)
-    }
-    async min(params){
-        return await this.handle('min',params)
-    }
-    async sum(params){
-        return await this.handle('sum',params)
-    }
     async create(params){
-        return await this.handle('create',params)
+        return await this.model.handle('create',params)
     }
     async bulkCreate(params){
-        return await this.handle('bulkCreate',params)
+        return await this.model.handle('bulkCreate',params)
     }
-    async delete(params){
-        return await this.handle('delete',params)
+    async save(){
+        return await this.dao.save()
     }
-    async deleteById(params){
-        let model= await this.handle('findById',params)
-        model.destroy();
+    async update(params){
+        return await this.dao.update(params)
     }
-
-    async query(params){
-        this.database.query(params)
+    async destroy(params){
+        return await this.dao.destroy(params)
     }
-    //合并数据库操作添加到事物中
-    async transaction(dataBaseFn){
-        let transaction=await this.database.transaction()
-        try {
-             await dataBaseFn();
-        }catch (e) {
-            return transaction.rollback()
-        }
-        return transaction.commit()
+    async reload(params){
+        return await this.dao.reload(params)
+    }
+    async increment(params){
+        return await this.dao.increment(params)
+    }
+    async decrement(params){
+        return await this.dao.decrement(params)
     }
 }
